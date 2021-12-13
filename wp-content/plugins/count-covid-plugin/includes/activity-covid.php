12ad596f7bb1c1d6ebdbad484caa80cfe9aadca9
Naufal <muh.naufal@hotmail.com>
2021-12-13 16:20:01 +0700

    reapit plugin

--- wp-content/plugins/count-covid-plugin/includes/activity-covid.php
+++ wp-content/plugins/count-covid-plugin/includes/activity-covid.php
@@ -1,4 +1,18 @@
 <div class="wrap">
- <h1>Halo!</h1>
- <p>Ini adalah halaman plugin pertamaku</p>
+ <h1>Setting Configuration Plugin Count Covid!</h1>
+<p>Pengaturan ini belum dapat digunakan</p>
+			
+ <form action="" method="post">
+     <label for="endpoint">API URL</label>
+     <input type="text" name="endpoint">
+     <label for="period">Periode</label>
+     <select name="period" id="period">
+         <option value="7">7 Menit</option>
+         <option value="14">14 Menit</option>
+         <option value="30">30 Menit</option>
+     </select>
+    <p class="submit">
+        <button type="submit" name="Submit" value="">Simpan</button>
+    </p>
+ </form>
 </div>
--- wp-content/plugins/count-covid-plugin/plugin-covid-update.php
+++ wp-content/plugins/count-covid-plugin/plugin-covid-update.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Plugin Count Covid
  * Plugin URI: http://localhost/count-covid
- * Description: The very first plugin that I have ever created.
+ * Description: The first plugin that I have ever created.
  * Version: 1.0
  * Author: Naufal
  * Author URI: http://naufalols.github.io
@@ -22,15 +22,13 @@ register_activation_hook(__FILE__, 'create_table_cron_plugin');
 register_activation_hook(__FILE__, 'save_to_database_cron');
 register_activation_hook(__FILE__, 'run_on_activate');
 
-// add_action('save_data_covid_to_database', 'save_to_database');
 add_action('save_data_covid_to_database', 'api_covid');
 
-// add_filter('the_content', 'api_covid');
 
 
 function run_on_activate()
 {
-    if (!wp_next_scheduled('get_data_covid')) {
+    if (!wp_next_scheduled('save_data_covid_to_database')) {
         wp_schedule_event(time(), 'every_three_minutes', 'save_data_covid_to_database');
     }
 }
@@ -56,12 +54,12 @@ add_filter('cron_schedules', 'setting_cron_get_api');
 
 
 
-function api_covid()
+function api_covid($content)
 {
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://covid19.mathdro.id/api/confirmed',
+        CURLOPT_URL => 'http://covid19.mathdro.id/api/confirmed',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -74,40 +72,35 @@ function api_covid()
         ),
     ));
 
+
     $response = curl_exec($curl);
+    $err = curl_error($curl);
 
     curl_close($curl);
-    // echo $response;
     global $wpdb;
     $table_name = $wpdb->prefix . 'covidCount';
 
-    // return $response;
     $data = json_decode($response);
-    if (!empty($data)) {
-        // $dataCovid = array();
-        foreach ($data as $key => $value) {
-            $wpdb->insert(
-                $table_name,
-                array(
+
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        if (!empty($data)) {
+            foreach ($data as $key => $value) {
+                $wpdb->insert(
+                    $table_name,
+                    array(
                     'nama_negara'       => $value->countryRegion,
                     'kasus_aktif'       => $value->confirmed,
                     'kasus_meninggal'   => $value->deaths,
                     'kasus_sembuh'      => $value->recovered,
                     'updated_at'        => date('Y-m-d H:i:s')
                 )
-            );
+                );
+            }
         }
-        // $wpdb->insert(
-        //     $table_name,
-        //     $dataCovid
-        // );
-
-        // print_r($dataCovid);
     }
-
-
-
-    // print_r($dataCovid);
 }
 
 function save_to_database()
@@ -131,15 +124,36 @@ function save_to_database()
 function save_to_database_cron()
 {
     global $wpdb;
+    $endpoint = 'https://covid19.mathdro.id/api';
     $table_name = $wpdb->prefix . 'cronPlugin';
 
-    $wpdb->insert(
-        $table_name,
-        array(
-            
-            'api_endpoint'       => ' https://covid19.mathdro.id/api',
+    $checkIfExists = $wpdb->get_var("SELECT * FROM $table_name WHERE api_endpoint = '$endpoint'");
+
+    if ($checkIfExists === null) {
+        $wpdb->insert(
+            $table_name,
+            array(
+            'api_endpoint'      => $endpoint,
             'period_sync'       => 7,
             'updated_at'        =>date('Y-m-d H:i:s')
         )
-    );
+        );
+    } else {
+        $wpdb->update(
+            $table_name,
+            array(
+                
+                'api_endpoint'      => 'https://covid19.mathdro.id/api',
+                'period_sync'       => 7,
+                'updated_at'        =>date('Y-m-d H:i:s')
+            ),
+            array( 'id' => 1 ),
+            array(
+                '%s',
+                '%d',
+                '%s'
+            ),
+            array('%d')
+        );
+    }
 }
